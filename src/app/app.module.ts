dcfcc2a969f31681f26c344b9fad0dd9e2bf8289
olexiychebotkevich <olexiychebotkevich@A33COMP4>
2019-03-27 17:48:47 +0200

    new changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WorkerComponent } from './worker/worker.component';
 
+import { WorkersService } from './workers.service';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { MatButtonModule, MatMenuModule, MatDatepickerModule } from '@angular/material';
+import { MatNativeDateModule, MatIconModule, MatCardModule } from '@angular/material';
+import { MatSidenavModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatTooltipModule, MatToolbarModule } from '@angular/material';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,9 +25,29 @@ import { WorkerComponent } from './worker/worker.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatPaginatorModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTooltipModule,
+    MatToolbarModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HttpClientModule,
+    WorkersService,
+    MatDatepickerModule
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/workers.service.ts
+++ src/app/workers.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Worker } from './worker';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkersService {
+  url = 'http://localhost:54445/api/workers';
+  httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'}) };
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAllWorkers(): Observable<Worker[]> {
+    return this.http.get<Worker[]>(this.url + '/ReadAllWorkersInfo');
+  }
+
+  getWorkerById(WorkerId: string): Observable<Worker> {
+    return this.http.get<Worker>(this.url + '/ReadWorkerById/' + WorkerId);
+  }
+
+  createWorker(worker: Worker): Observable<Worker> {
+    return this.http.post<Worker>(this.url + '/CreateWorker/', worker, this.httpOptions);
+  }
+
+  updateWorker(worker: Worker): Observable<Worker> {
+    return this.http.put<Worker>(this.url + '/UpdateWorker/', worker, this.httpOptions);
+  }
+
+  deleteWorkerById(workerId: string): Observable<number> {
+    return this.http.delete<number>(this.url + '/DeleteWorkerById/' + workerId, this.httpOptions);
+  }
 }
